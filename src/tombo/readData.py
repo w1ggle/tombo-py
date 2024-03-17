92a7042d8534a0367f6ff6f80cdb19eff29a2c89
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2024-03-17 17:32:10 -0400

    output as text file

--- src/tombo/readData.py
+++ src/tombo/readData.py
@@ -3,7 +3,8 @@ import os
 
 data = np.load(os.path.join( os.getcwd(), '..', '..', 'output\data\wake\wake_0.npz' ))
 lst = data.files
-for item in lst:
-    print(item)
-    print(data[item])
-print(data)
+with open("output.txt", "a") as f:
+    for item in lst:
+        print(item, file=f)
+        print(data[item], file=f)
+#print(data)
